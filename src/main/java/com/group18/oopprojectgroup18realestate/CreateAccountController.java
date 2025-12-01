b00ad9b3271d54d4882a156fbd63dde67ab7d31e
sahkib159 <sahkib159@gmail.com>
2025-12-01 07:17:46 +0600

    LogIn & Create New Account skeleton

---
+++ src/main/java/com/group18/oopprojectgroup18realestate/CreateAccountController.java
@@ -0,0 +1,23 @@
+package com.group18.oopprojectgroup18realestate;
+
+public class CreateAccountController
+{
+    @javafx.fxml.FXML
+    private TextField newUsernameTextField;
+    @javafx.fxml.FXML
+    private TextField newPasswordTextField;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void newRoleComboBox(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void returnOnClick(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/group18/oopprojectgroup18realestate/LogInController.java
+++ src/main/java/com/group18/oopprojectgroup18realestate/LogInController.java
@@ -3,5 +3,25 @@ package com.group18.oopprojectgroup18realestate;
 public class LogInController
 {
     @javafx.fxml.FXML
+    private TextField usernameField;
+    @javafx.fxml.FXML
+    private TextField passwordField;
+    @javafx.fxml.FXML
+    private Label errorLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logInOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void roleComboBox(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createAccountOnClick(ActionEvent actionEvent) {
+    }
+}
