f6d3adde02ac9ea438af6f3fac622fa94bb7da85
ramisajahan <ramisajahan18@gmail.com>
2025-12-04 19:59:44 +0600

    propertmanagement controller and searchproperty controller with file handling with properties bin

---
+++ src/main/java/com/group18/oopprojectgroup18realestate/Ramisa/propertySearchController.java
@@ -0,0 +1,158 @@
+package com.group18.oopprojectgroup18realestate.Ramisa;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+
+public class propertySearchController
+{
+    @javafx.fxml.FXML
+    private TableColumn<propertymanagement,String> typeCol;
+    @javafx.fxml.FXML
+    private Label messageL;
+    @javafx.fxml.FXML
+    private TextField locationTF;
+    @javafx.fxml.FXML
+    private TableColumn<propertymanagement,String> propertyIDCol;
+    @javafx.fxml.FXML
+    private TableColumn<propertymanagement,String> statusCol;
+    @javafx.fxml.FXML
+    private ComboBox<String> typeCB;
+    @javafx.fxml.FXML
+    private Label propertydetailsL;
+    @javafx.fxml.FXML
+    private TableView<propertymanagement> searchPropertyTableview;
+    @javafx.fxml.FXML
+    private TextField budgetTF;
+    @javafx.fxml.FXML
+    private TableColumn<propertymanagement,String> LocationCol;
+    @javafx.fxml.FXML
+    private TableColumn<propertymanagement,String> priceCol;
+
+
+    private ArrayList<propertymanagement> allProperties = new ArrayList<>();
+
+
+    @javafx.fxml.FXML
+    public void initialize() {
+        typeCB.getItems().addAll("All", "Apartment", "House", "Flat", "Commercial");
+        typeCB.setValue("All");
+
+        propertyIDCol.setCellValueFactory(new PropertyValueFactory<>("propertyID"));
+        LocationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("propertyType"));
+        priceCol.setCellValueFactory(new PropertyValueFactory<>("rentPrice"));
+
+        loadButtonOC(null);
+
+    }
+
+    @javafx.fxml.FXML
+    public void bookPropertyOnClickButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchPropertyOnClick(ActionEvent actionEvent) {
+        String location = locationTF.getText().strip();
+        String type = typeCB.getValue();
+        String budgetText = budgetTF.getText().strip();
+
+        searchPropertyTableview.getItems().clear();
+        searchPropertyTableview.getItems().addAll(allProperties);
+
+        if (location.isEmpty() || budgetText.isEmpty() || type == null) {
+            messageL.setText("fill all information.");
+            return;
+
+
+        }
+
+        // validation (budget)
+        double budget;
+        try {
+            budget = Double.parseDouble(budgetText);
+            if (budget <= 0) {
+                messageL.setText("budget should positive!");
+                return;
+            }
+        } catch (NumberFormatException e) {
+            messageL.setText("budget amount invalid!");
+            return;
+        }
+
+        int count = 0;
+        for (propertymanagement property : allProperties) {
+            boolean matchLocation = location.isEmpty() ||
+                property.getLocation().equalsIgnoreCase(location);
+
+
+            boolean matchType = type != null || !type.equals("All");
+                property.getPropertyType().equalsIgnoreCase(type);
+
+            boolean matchBudget = budgetText.isEmpty() ||
+                property.getRentPrice() <= budget;
+
+            boolean isAvailable = property.getStatus().equalsIgnoreCase("Available");
+
+            if (matchLocation || matchType || matchBudget || isAvailable) {
+                searchPropertyTableview.getItems().add(property);
+
+                count++;
+            }
+        }messageL.setText("Found: " + count);
+
+        if (count == 0) {
+            messageL.setText("No properties found!");
+        } else {messageL.setText(" Found property");
+        }
+
+    }
+
+
+    @javafx.fxml.FXML
+    public void loadButtonOC(ActionEvent actionEvent) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("properties.bin"))) {
+            allProperties = (ArrayList<propertymanagement>) in.readObject();
+
+            // Available properties ONLY
+            ArrayList<propertymanagement> availableProperties = new ArrayList<>();
+            for (propertymanagement p : allProperties) {
+                if (p.getStatus().equals("Available")) {
+                    availableProperties.add(p);
+                }
+            }
+            searchPropertyTableview.getItems().clear();
+            for (propertymanagement p : availableProperties) {
+                searchPropertyTableview.getItems().add(p);
+            }
+
+            messageL.setText("Loaded available property!");
+
+        } catch (FileNotFoundException e) {
+            messageL.setText("No saved file found!");
+
+        } catch (Exception e) {
+            messageL.setText("Error loading properties!");
+        }
+    }
+    @javafx.fxml.FXML
+    public void backButtonOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("tenantDashBoard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage =(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
