a2bba1c48f868c9c11926a0be019a366b056b4b7
sahkib159 <sahkib159@gmail.com>
2025-12-05 02:13:32 +0600

    Ramisha's Scene Switch fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,19 @@
 module com.group18.oopprojectgroup18realestate {
+
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.group18.oopprojectgroup18realestate to javafx.fxml;
     opens com.group18.oopprojectgroup18realestate.Sahkib.SystemAdministrator to javafx.fxml;
-    exports com.group18.oopprojectgroup18realestate.Sahkib.SystemAdministrator;
     opens com.group18.oopprojectgroup18realestate.Sahkib.FinanceOfficer to javafx.fxml;
-    exports com.group18.oopprojectgroup18realestate.Sahkib.FinanceOfficer;
+
+    // ADD THESE FOR R A M I S A
+    opens com.group18.oopprojectgroup18realestate.Ramisa to javafx.fxml;
 
     exports com.group18.oopprojectgroup18realestate;
-    opens com.group18.oopprojectgroup18realestate.Sonda.CustomerSupport to javafx.fxml;
-    exports com.group18.oopprojectgroup18realestate.Sonda.CustomerSupport;
-    opens com.group18.oopprojectgroup18realestate.Sonda.MarketingOfficer to javafx.fxml;
-    exports com.group18.oopprojectgroup18realestate.Sonda.MarketingOfficer;
+    exports com.group18.oopprojectgroup18realestate.Sahkib.SystemAdministrator;
+    exports com.group18.oopprojectgroup18realestate.Sahkib.FinanceOfficer;
+
+    // OPTIONAL but safe
+    exports com.group18.oopprojectgroup18realestate.Ramisa;
 }
